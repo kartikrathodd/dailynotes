b862a0910d6d7bcf0b644593cea1f35f0fcebc71
kartikrathodd <140259113+kartikrathodd@users.noreply.github.com>
2025-08-26 01:00:48 +0530

    Update server.js

--- server.js
+++ server.js
@@ -12,6 +12,9 @@ app.use(express.static(path.join(__dirname, "public")));
 const rooms = {};
 
 io.on("connection", (socket) => {
+  console.log("A user connected:", socket.id);
+
+  // Join a room
   socket.on("joinRoom", (room) => {
     socket.join(room);
     if (!rooms[room]) rooms[room] = new Set();
@@ -19,22 +22,26 @@ io.on("connection", (socket) => {
     io.to(room).emit("participants", rooms[room].size);
   });
 
+  // Chat message
   socket.on("chat message", (msg) => {
     io.to(msg.room).emit("chat message", msg);
   });
 
+  // Seen message
   socket.on("seen", (data) => {
     io.to(data.room).emit("seen");
   });
 
+  // Typing indicators
   socket.on("typing", (room) => {
-    socket.to(room).emit("typing");
+    socket.to(room).emit("typing", { id: socket.id });
   });
 
   socket.on("stopTyping", (room) => {
-    socket.to(room).emit("stopTyping");
+    socket.to(room).emit("stopTyping", { id: socket.id });
   });
 
+  // Disconnecting
   socket.on("disconnecting", () => {
     for (let room of socket.rooms) {
       if (rooms[room]) {
